5a96fb048977fe475cba269b7240d04775ca2909
irfanlone <irfan.mf69@gmail.com>
2016-03-05 17:05:18 -0800

    Initial Commit

---
+++ DiningPlaces/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  DiningPlaces
+//
+//  Created by Irfan Lone on 3/5/16.
+//  Copyright © 2016 Yuzu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ DiningPlaces/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  DiningPlaces
+//
+//  Created by Irfan Lone on 3/5/16.
+//  Copyright © 2016 Yuzu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
