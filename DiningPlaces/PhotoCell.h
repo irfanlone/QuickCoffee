0b19e79231a2833df096c08c53733427a385aad3
irfanlone <irfan.mf69@gmail.com>
2016-03-06 16:13:02 -0800

    display list of photos for the venue in the detail view

---
+++ DiningPlaces/PhotoCell.h
@@ -0,0 +1,13 @@
+//
+//  PhotoCell.h
+//  DiningPlaces
+//
+//  Created by Irfan Lone on 3/6/16.
+//  Copyright © 2016 Yuzu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoCell : UICollectionViewCell
+
+@end
---
+++ DiningPlaces/PhotoCell.m
@@ -0,0 +1,13 @@
+//
+//  PhotoCell.m
+//  DiningPlaces
+//
+//  Created by Irfan Lone on 3/6/16.
+//  Copyright © 2016 Yuzu. All rights reserved.
+//
+
+#import "PhotoCell.h"
+
+@implementation PhotoCell
+
+@end
